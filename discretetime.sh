c25e124d110a4d2ea37e3099bf9c523a0eff5ea5
Albert Visagie <avisagie@gmail.com>
2012-08-10 11:56:07 +0200

    set period in startup script

--- discretetime.sh
+++ discretetime.sh
@@ -4,5 +4,5 @@ DIR=`dirname $0`
 
 cd $DIR
 
-java -Djava.util.logging.config.file=logging.properties -ms16m -mx16m -jar discretetime-0.2.jar & 
+java -Dperiod=600000 -Djava.util.logging.config.file=logging.properties -ms16m -mx16m -jar discretetime-0.2.jar & 
 disown
